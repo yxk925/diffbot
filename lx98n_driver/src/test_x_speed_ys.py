89f46d332947cc72be39ffa430c4d586552407dc
Alexander R Young <yxk925@hotmail.com>
2025-09-17 22:01:49 +0800

    Commit all files about some  localization methods

---
+++ lx98n_driver/src/test_x_speed_ys.py
@@ -0,0 +1,25 @@
+
+import sys
+import time
+
+from lx98n_driver_wiringpi import Lx98N_MotorDriverWiringpi
+        
+def motor_driver_test_basic(PWM_VALUE, sec):
+    print("test PWM:%d,  %d seconds", PWM_VALUE, sec)
+    motor = Lx98N_MotorDriverWiringpi()
+
+    motor.MotorSpeedSetAB(pwm_value, pwm_value)
+    time.sleep(sec)
+    
+    motor.stop()
+
+    
+
+if __name__ == "__main__":
+    print("Starting Motor DRiver Test")
+    pwm_value = int(sys.argv[1])
+    sec = int(sys.argv[2])
+    
+    motor_driver_test_basic(pwm_value, sec)
+    print("Starting Motor DRiver Test...END")
+
